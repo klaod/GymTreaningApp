382cfb27fa5b3ba17cc204f4810e8c6328c867d4
klaod <reznichenkovo@gmail.com>
2018-01-29 22:31:05 +0200

    added day component

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { DayComponent } from './day/day.component';
 
 @NgModule({
   imports: [
     SharedModule
   ],
-  declarations: [CalendarComponent]
+  declarations: [CalendarComponent, DayComponent]
 })
 export class CalendarModule { }
